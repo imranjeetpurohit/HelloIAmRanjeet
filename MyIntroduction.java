aa40425c7dbd844b3a29bd14464d15464bb070d7
Ranjeet Raj <ranjeet.raj@streebo.com>
2025-04-16 10:01:05 +0530

    Initial

--- MyIntroduction.java
+++ MyIntroduction.java
@@ -1,7 +1,7 @@
 public class MyIntroduction {
     public static void main(String[] args) {
         System.out.println("👋 Hello, World!");
-        System.out.println("My name is Ranjeet."); // Replace with your name
+        System.out.println("My name is Ranjeet Raj."); // Replace with your name
         System.out.println("I'm a software engineer who loves Java, tech, and building cool stuff.");
         System.out.println("This is my first public project on GitHub as a small intro.");
         System.out.println("Stay tuned for more!");
